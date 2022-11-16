ff448b14a52ad32076f43547643cf03494fc066a
eknaprasath <supremeprasath@gmail.com>
2022-11-16 11:02:29 +0000

    added version

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,6 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   version = "2.8.0"
   bucket_prefix = var.prefix
-  bucket = "my-s3-bucket"
   acl    = "private"
 
   versioning = {
